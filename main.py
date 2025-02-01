4265f8f20b4b1613ca0dc42e6142949541e5b6d2
Lian Arthofer <77974438+LianArthofer@users.noreply.github.com>
2025-02-01 20:49:32 +0000

    edits

--- main.py
+++ main.py
@@ -4,57 +4,56 @@ from langchain_anthropic import ChatAnthropic
 from langchain_core.messages import HumanMessage, SystemMessage
 import yaml
 
-
 if not os.environ.get("ANTHROPIC_API_KEY"):
   os.environ["ANTHROPIC_API_KEY"] = getpass.getpass("Enter API key for Anthropic: ")
   # Load the YAML file
 with open("event.yml", "r") as file:
     prompt_data = yaml.safe_load(file)
 
-
 model = ChatAnthropic(model="claude-3-5-sonnet-20240620")
 
 
-
-
-# Define the initial system message
-messages = [
-    SystemMessage(str(prompt_data))
-]
-
-# Start the interactive loop
-print("Welcome to the game! You are talking to as android")
-print("Type 'exit' to end the game.\n")
-
 # Get the AI's response
-map = model.invoke([HumanMessage(content="make me ive me a maze (nxn) with wall char '*' and path char '.'")])
-print(map)
+#map = model.invoke([HumanMessage(content="make me ive me a maze (nxn) with wall char '*' and path char '.'")])
+#print(map)
 
+print("Type 'exit' to end the game.\n\n")
+
+
+i=0
 #Main Loop
-while True:
-    # Exit the game if the user types 'exit'
-    if user_input.lower() == "exit":
-        print("Goodbye! The game has ended.")
-        break
+while i < 3:
+    messages = []
+    aimessages = []
+    i = i + 1
+    act_key = f"act{i}"
+    context = [
+    SystemMessage(str({**prompt_data['prompt']['global'], **prompt_data['prompt'][act_key]})),
+    HumanMessage("hello"),
+    ]
+    print(model.invoke(context).content)
+    while True:
+        # Get user input
+        user_input = input("You: ")
+        # Exit the game if the user types 'exit'
+        
+        #this message is processed to the pther model
+        messages.append(HumanMessage(content=user_input).content)
 
+        # Get the AI's response
+        response = model.invoke(messages)
 
-    # Get user input
-    user_input = input("You: ")
+        # Print the AI's response
+        print(f"Android: {response.content}\n")
 
-    # Add the user's message to the conversation
-    #this message is processed to the pther model
-    messages.append(HumanMessage(content=user_input))
+        # Add the AI's response to the conversation
+        aimessages.append(response.content)
 
+        #send_data(response, messages, prompt_data)e
 
-    # Get the AI's response
-    response = model.invoke(messages)
-
-    # Print the AI's response
-    print(f"Android: {response.content}\n")
-    print(response)
-
-    # Add the AI's response to the conversation
-    messages.append(response)
-
-    send_data(response, messages, prompt_data)
-
+        if user_input.lower() == "exit":
+            print("Goodbye! The game has ended.")
+            break
+        print(aimessages)
+        print(messages)
+        print(prompt_data['prompt'][act_key]['act_context'])
